a8468dddb340125ad7678d1c6107058ec00364a5
Evan Raab <eraab953@gmail.com>
2020-09-10 13:17:52 -0400

    Fixed Commit

---
+++ Assets/Scripts/DeathFromHeight.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathFromHeight : MonoBehaviour {
+	
+	public int deathHeight = 0;
+	private Rigidbody rb;
+	public GameObject soundKill;
+
+
+	
+
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        	rb = GetComponent<Rigidbody>();
+			soundKill = GameObject.FindWithTag("sounds");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rb.transform.position.y < 0) {
+			Destroy(soundKill);
+			LevelTrack.levelNumber = 0;
+			SceneManager.LoadScene("GameOver");
+			
+		}
+    }
+}
---
+++ Assets/Scripts/LevelTrack.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+public class LevelTrack : MonoBehaviour {
+	
+	private Text text;
+	public static int levelNumber = 0;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<Text>();
+		levelNumber++;
+		
+	}
+	
+
+    // Update is called once per frame
+    void Update()
+    {
+	   
+		
+		  text.text = "Level: " + levelNumber;
+    }
+}
